eea51206f636fbf1f96b0f8d7276c9a4287d3b55
Noah Harris <dakoolstwunn@gmail.com>
2020-08-11 01:55:52 -0400

    Update tests
    Write tests for new handler readBan
    Add casses for createBan
    Add tests for createBan that should fail the request with a 400

--- bans_test.go
+++ bans_test.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -75,6 +76,23 @@ func Test_createBan(test *testing.T) {
 		}),
 		mustMarshal(map[string]interface{}{
 			"banned": uuid.New().String(),
+			"reason": strings.Repeat(".", 255),
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned": uuid.New().String(),
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned":   uuid.New().String(),
+			"duration": 42069,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned":   uuid.New().String(),
+			"forever":  true,
+			"duration": 42069,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned":   uuid.New().String(),
+			"duration": 2 << 31,
 		}),
 	}
 
@@ -104,6 +122,79 @@ func Test_createBan(test *testing.T) {
 		ban.FromMap(r_map["ban"].(map[string]interface{}))
 		if err = banOK(banner, ban); err != nil {
 			test.Fatal(err)
+		}
+	}
+}
+
+func Test_createBan_badRequest(test *testing.T) {
+	var set []byte
+	var sets [][]byte = [][]byte{
+		mustMarshal(map[string]interface{}{
+			"banned":  "nobody",
+			"reason":  "They enjoy pokemon sword and / or shield",
+			"forever": true,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned": uuid.New().String(),
+			"reason": strings.Repeat(".", 256),
+		}),
+		mustMarshal(map[string]interface{}{
+			"forever": true,
+		}),
+		mustMarshal(map[string]interface{}{
+			"reason": "I don't really know",
+		}),
+		mustMarshal(map[string]interface{}{
+			"reason":   "I don't really know",
+			"duration": 666,
+		}),
+		mustMarshal(map[string]interface{}{
+			"reason":  "I don't really know",
+			"forever": true,
+		}),
+		mustMarshal(map[string]interface{}{
+			"reason":  "I don't really know",
+			"forever": 1 << 1,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned": uuid.New().String(),
+			"reason": 11,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned":   uuid.New().String(),
+			"duration": "forever and a half",
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned":  uuid.New().String(),
+			"forever": 4,
+		}),
+		mustMarshal(map[string]interface{}{
+			"banned": 0,
+		}),
+		mustMarshal(map[string]interface{}{}),
+		[]byte("Look at me! I'm in your API! I'm in the Vault! Oh nevermind, they banned me..."),
+		nil,
+	}
+
+	var code int
+	var err error
+
+	var request *http.Request
+	var valued context.Context
+
+	for _, set = range sets {
+		valued = context.WithValue(context.TODO(), "requester", banner.ID)
+		if request, err = http.NewRequestWithContext(valued, "POST", "/", bytes.NewReader(set)); err != nil {
+			test.Fatal(err)
+		}
+
+		if code, _, err = createBan(request); err != nil {
+			test.Fatal(err)
+		}
+
+		if code != 400 {
+			test.Errorf("%s", string(set))
+			test.Errorf("got code %d", code)
 		}
 	}
 }
@@ -139,5 +230,64 @@ func Test_createBan_wasBanned(test *testing.T) {
 
 	if !isBanned {
 		test.Errorf("%s was not banned!", banned)
+	}
+}
+
+func Test_readBan(test *testing.T) {
+	var ban monketype.Ban = monketype.NewBan(
+		banner.ID,
+		uuid.New().String(),
+		"Does not like splatoon",
+		0,
+		false,
+	)
+
+	var err error
+	if err = monkebase.WriteBan(ban.Map()); err != nil {
+		test.Fatal(err)
+	}
+
+	var request *http.Request
+	if request, err = http.NewRequest("GET", "/id/"+ban.ID, nil); err != nil {
+		test.Fatal(err)
+	}
+
+	var code int
+	var r_map map[string]interface{}
+	if code, r_map, err = readBan(request); err != nil {
+		test.Fatal(err)
+	}
+
+	if code != 200 {
+		test.Errorf("got code %d", code)
+	}
+
+	var fetched monketype.Ban
+	fetched.FromMap(r_map["ban"].(map[string]interface{}))
+
+	if fetched.ID != ban.ID {
+		test.Errorf("id mismatch! have: %s, want: %s", fetched.ID, ban.ID)
+	}
+
+	if fetched.Banned != ban.Banned {
+		test.Errorf("banned mismatch! have: %s, want: %s", fetched.Banned, ban.Banned)
+
+	}
+}
+
+func Test_readBan_nosuchban(test *testing.T) {
+	var request *http.Request
+	var err error
+	if request, err = http.NewRequest("GET", "/id/foobar", nil); err != nil {
+		test.Fatal(err)
+	}
+
+	var code int
+	if code, _, err = readBan(request); err != nil {
+		test.Fatal(err)
+	}
+
+	if code != 404 {
+		test.Errorf("got code %d", code)
 	}
 }
